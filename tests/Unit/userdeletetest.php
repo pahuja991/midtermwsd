f5d867341187e527f8c7b049e4132840920fcaed
pahuja991 <rp824@njit.edu>
2018-11-03 09:53:29 -0400

    Fix:Unittest-deleteuser

--- tests/Unit/userdeletetest.php
+++ tests/Unit/userdeletetest.php
@@ -4,7 +4,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
-class DeleteUserTest extends TestCase
+class userdeletetest extends TestCase
 {
     /**
      * A basic test example.
